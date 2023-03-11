d5fbc9a5e5a2a19da1fbbdc331ded29790a41132
Fran <franciscorualesp15@gmail.com>
2023-03-10 23:53:12 -0500

    menuItem jsx componente creado

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -3,8 +3,13 @@ import React from 'react';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div
+    className='app__specialMenu flex__center section__padding'
+    id='menu'
+  >
+    <div className='app__specialMenu-title'>
+      <h1 className='headtext__cormorant'>Today&apos;s Special</h1>
+    </div>
   </div>
 );
 
